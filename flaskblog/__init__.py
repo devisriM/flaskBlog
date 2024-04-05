488d17bca60d6d6a6f62940ad1b3b82faf3abe1b
devisri <devisrimanavarthi@outlook.com>
2024-04-05 12:01:25 -0700

    added login authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b426a36e2860adfb62bbb907936c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./site.db'
 db = SQLAlchemy(app)
+
+# Flask SQLAlchemy db.create_all() | error: (working outside of application context)
+# with app.app_context():
+#     db.create_all()
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
